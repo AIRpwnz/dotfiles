b041bbbe58c2de71f3bc02920795fdf1c1e704df
AIRpwnz <air@uhost.ovh>
2026-02-23 16:01:00 +0200

    convert fish aliases to abbreviations and update prompt

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,3 +1,4 @@
+# General Settings
 set -g fish_greeting ""
 set -gx LANG en_US.UTF-8
 set -gx EDITOR hx
@@ -9,27 +10,33 @@ set -gx HTTPSTAT_SHOW_SPEED true
 set -gx K9S_CONFIG_DIR "$HOME/.config/k9s"
 set -gx ANSIBLE_MAX_DIFF_SIZE 1048576
 
+# Path Configuration
 fish_add_path /opt/homebrew/bin /opt/homebrew/sbin $HOME/.local/bin $HOME/.local/share/uv/python/bin
 
+# Local Secrets
 if test -f $HOME/.fish_secrets
     source $HOME/.fish_secrets
 end
 
+# Interactive Session Configuration
 if status is-interactive
+    # Shell Prompt
     starship init fish | source
 
-    alias ks="k9s"
-    alias cat="bat"
-    alias yz="y"
-    alias h="hx"
-    alias ff="fastfetch"
-    alias ls="eza --color=always --long --git --icons=always"
-    alias lue="lumen explain"
-    alias lud="lumen draft"
+    # Abbreviations
+    abbr ks k9s
+    abbr cat bat
+    abbr yz y
+    abbr h hx
+    abbr ff fastfetch
+    abbr ls eza --color=always --long --git --icons=always
+    abbr lue lumen explain
+    abbr lud lumen draft
 
+    # Tool Integrations
     fzf --fish | source
-
     zoxide init fish | source
 
+    # Startup Info
     fastfetch
 end
